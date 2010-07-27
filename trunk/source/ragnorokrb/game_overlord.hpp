10ef8906ee351ed82499d6b83abda6844b8a11a6
rpg.ragnarok.game <rpg.ragnarok.game@b9f03f81-d838-4a7b-86f3-b03b787f155d>
2010-07-27 09:59:55 +0000

    REAL Revision 1. Nothing is really working, just some bit of a skeleton. Only save files working. I am recoding the rest of it from reference.
    rev001

---
+++ trunk/source/ragnorokrb/game_overlord.hpp
@@ -0,0 +1,24 @@
+#ifndef GAME_OVERLORD_HPP_INCLUDED
+ #define GAME_OVERLORD_HPP_INCLUDED
+ #include "game_entities.h"
+using namespace std;
+class overlord {
+  public:
+   overlord();
+   ~overlord();
+   std::vector <entity> entity_list;
+   void test();
+};
+overlord::overlord() {
+
+}
+overlord::~overlord() {
+
+}
+void overlord::test() {
+entity * Entity = new entity("Entity0");
+entity * Entitys = new entity("Entity0");
+}
+
+
+#endif // GAME_OVERLORD_HPP_INCLUDED
