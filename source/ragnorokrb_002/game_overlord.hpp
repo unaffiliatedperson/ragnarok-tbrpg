bf655a6132f71720fe4f8b30ef6ff97a860d9a38
rpg.ragnarok.game <rpg.ragnarok.game@b9f03f81-d838-4a7b-86f3-b03b787f155d>
2010-07-28 02:13:38 +0000

    Revision 002. Entities now is a working skeleton with overlord able to operate them.

---
+++ source/ragnorokrb_002/game_overlord.hpp
@@ -0,0 +1,25 @@
+#ifndef GAME_OVERLORD_HPP_INCLUDED
+ #define GAME_OVERLORD_HPP_INCLUDED
+ #include "game_entities.h"
+using namespace std;
+class overlord {
+  public:
+   overlord();
+   ~overlord();
+   std::vector <entity> entity_list;
+   void add_entity(entity*);
+};
+
+overlord::overlord() {
+
+}
+overlord::~overlord() {
+
+}
+
+void overlord::give_item(entity* x) {
+entity_list.push_back(*x);
+}
+
+
+#endif // GAME_OVERLORD_HPP_INCLUDED
