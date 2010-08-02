468df432a2b308b32937b7663978d03546446a1f
rpg.ragnarok.game <rpg.ragnarok.game@b9f03f81-d838-4a7b-86f3-b03b787f155d>
2010-08-02 06:24:34 +0000

    useless

--- trunk/source/ragnarok_backup_notworking/v7/game_overlord_attack.cpp
+++
@@ -1,6 +0,0 @@
-#include "game_overlord.hpp"
-
-void overlord::entity_attack(unsigned char entity1, unsigned char entity2) {
-short temp_health = entity_list.at(entity1).rtrn_health();
-std::cout << "Health " << temp_health << std::endl;
-}
--- trunk/source/ragnarok_backup_notworking/v7/game_saves_constructor.cpp
+++
@@ -1,6 +0,0 @@
-#include "game_saves.hpp"
-file_io::file_io(const char file_name[]) {
-    File_name = (char*) file_name;
-    has_ran = false;
-    has_ran_write = false;
-}
